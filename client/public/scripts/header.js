5a460815f8ea5f530b56756128d162e2684c810d
justinb1145 <45029075+justinb1145@users.noreply.github.com>
2024-09-26 17:49:36 -0700

    success

--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -6,8 +6,25 @@ headerContainer.className = 'header-container';
 const headerLeft = document.createElement('div');
 headerLeft.className = 'header-left';
 
-const headerLogo = document.createElement('img')
-headerLogo.src = '/logo.png'
+const headerLogo = document.createElement('img');
+headerLogo.src = '/logo.png';
 
+const headerTitle = document.createElement('h1');
+headerTitle.textContent = 'UnEarthed';
+
+const headerRight = document.createElement('div');
+headerRight.className = 'header-right';
+
+const headerButton = document.createElement('Home');
+headerButton.textContent = 'Home';
+
+headerButton.addEventListener('click', function handleClick(event) {
+    window.location = '/'
+  });
+
+headerContainer.appendChild(headerLeft);
+headerContainer.appendChild(headerRight);
+
+header.appendChild(headerContainer);
 
 
